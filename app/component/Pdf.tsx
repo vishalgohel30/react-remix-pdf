0f52655a428d7d82bff70f8d84336ae46502b92f
vishal gohel w <vishalgohel221296@gmail.com>
2024-03-27 18:35:17 +0530

    code update

--- app/component/Pdf.tsx
+++ app/component/Pdf.tsx
@@ -1,36 +1,8 @@
 // "use client";
 import { useEffect, useRef, useState } from 'react';
-import PSPDFKit from 'pspdfkit';
 
 function Index({document}) {
   const containerRef = useRef<HTMLDivElement | null>(null);
-
-	// useEffect(() => {
-	// 		const container = containerRef.current;
-
-	// 	if(document){
-	// 		let instance, PSPDFKit;
-	// 		(async function () {
-	// 			PSPDFKit = (await import('pspdfkit')).default;
-	// 			instance = await PSPDFKit.load({
-	// 				container,
-	// 				document,
-	// 				baseUrl: `${window.location.protocol}//${window.location.host}/`,
-	// 				// licenseKey: 'YOUR_LICENSE_KEY_GOES_HERE',
-	// 			});
-	// 		})();
-    //         // PSPDFKit && PSPDFKit.unload(container);
-    //         // setTimeout(()=>{
-    //         //     console.log('calll')
-    //         //     PSPDFKit && PSPDFKit.unload(container);
-    //         // },6000)
-	// 		return () => PSPDFKit && PSPDFKit.unload(container);
-		
-	// 	}
-
-      
-	// }, [document]);
-
     useEffect(() => {
 		if(document){
 			const container = containerRef.current;
@@ -54,10 +26,7 @@ function Index({document}) {
 
 const PdfList = [
 	{name:'pdf1', document:"https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf"},
-	{name:'pdf2', document:"https://www.africau.edu/images/default/sample.pdf"},
-	{name:'pdf3', document:"https://www.clickdimensions.com/links/TestPDFfile.pdf"},
-	{name:'pdf4', document:"https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"},
-	{name:'pdf5', document:"https://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf"},
+	{name:'pdf2', document:"https://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf"},
 ]
 
 const PdfView = () =>{
